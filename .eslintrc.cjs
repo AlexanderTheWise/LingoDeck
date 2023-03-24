b0db19858fa00eec628cb05eaeeb7433876a8380
AlexanderTheWise <aguillenhernandez@hotmail.com>
2023-03-24 16:44:38 +0100

    Install eslint and prettier, and add eslint configs

---
+++ .eslintrc.cjs
@@ -0,0 +1,28 @@
+module.exports = {
+  env: {
+    es2021: true,
+    node: true,
+  },
+  extends: ["xo", "prettier"],
+  overrides: [
+    {
+      extends: ["xo-typescript", "prettier"],
+      files: ["*.ts", "*.tsx"],
+      rules: {
+        "@typescript-eslint/consistent-type-definitions": [
+          "error",
+          "interface",
+        ],
+        "@typescript-eslint/naming-convention": "off",
+      },
+    },
+  ],
+  parserOptions: {
+    ecmaVersion: "latest",
+    sourceType: "module",
+  },
+  rules: {
+    "no-implicit-coercion": "off",
+    "new-cap": ["error", { capIsNewExceptions: ["Router"] }],
+  },
+};
