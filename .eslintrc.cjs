bd15af33900ba606c7e3e0c3e54116394fb489e2
AlexanderTheWise <aguillenhernandez@hotmail.com>
2023-03-24 21:31:56 +0100

    Test endpointNotFound middleware

--- .eslintrc.cjs
+++ .eslintrc.cjs
@@ -14,6 +14,7 @@ module.exports = {
           "interface",
         ],
         "@typescript-eslint/naming-convention": "off",
+        "@typescript-eslint/consistent-type-assertions": "off",
       },
     },
   ],
