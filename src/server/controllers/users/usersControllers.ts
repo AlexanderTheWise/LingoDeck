b65201ac8a225dbbeb8a95852401ac03103d4ede
AlexanderTheWise <aguillenhernandez@hotmail.com>
2023-03-25 19:44:18 +0100

    Add loginUser controller

---
+++ src/server/controllers/users/usersControllers.ts
@@ -0,0 +1,47 @@
+import "../../../loadEnvironment.js";
+import { type Request, type Response, type NextFunction } from "express";
+import bcrypt from "bcryptjs";
+import jwt from "jsonwebtoken";
+import User from "../../../database/models/User";
+import { type LoginUserPayload, type UserCredentials } from "../../types";
+import CustomError from "../../../CustomError/CustomError.js";
+
+export const loginUser = async (
+  request: Request<
+    Record<string, unknown>,
+    Record<string, unknown>,
+    UserCredentials
+  >,
+  response: Response,
+  next: NextFunction
+) => {
+  try {
+    const { username, password } = request.body;
+    const user = await User.findOne({ username }).exec();
+
+    if (!user) {
+      throw new Error("Couldn't find user with given name");
+    }
+
+    if (!(await bcrypt.compare(password, user.password))) {
+      throw new Error("Password doesn't match indicated");
+    }
+
+    const loginUserPayload: LoginUserPayload = {
+      id: user._id.toString(),
+      username: user.username,
+    };
+
+    const token = jwt.sign(loginUserPayload, process.env.JWT_SECRET!);
+
+    response.status(201).json({ token });
+  } catch (error) {
+    const wrongCredentials = new CustomError(
+      (error as Error).message,
+      401,
+      "Wrong credentials"
+    );
+
+    next(wrongCredentials);
+  }
+};
