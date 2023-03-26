a019a798bce4262f05374268819e68c1fddde28e
AlexanderTheWise <aguillenhernandez@hotmail.com>
2023-03-26 15:57:30 +0200

    Add morgan, express.json(), usersRouter and errors middlewares to chain

--- src/server/app.ts
+++ src/server/app.ts
@@ -1,6 +1,21 @@
 import express from "express";
+import morgan from "morgan";
+import {
+  endpointNotFound,
+  errorHandler,
+  validationError,
+} from "./middlewares/errorMiddlewares/errorMiddlewares.js";
+import usersRouter from "./routers/users/usersRouter.js";
 
 const app = express();
+
 app.disable("x-powered-by");
+app.use(morgan("dev"));
+app.use(express.json());
+
+app.use("/user", usersRouter);
+app.use(validationError);
+app.use(endpointNotFound);
+app.use(errorHandler);
 
 export default app;
