1cbe5eaf9cf735434fcceaad46bc1dda5f59e40d
AlexanderTheWise <aguillenhernandez@hotmail.com>
2023-06-08 19:06:17 +0200

    Add gzip compression, esbuild bundle and set node env to production

--- src/index.ts
+++ src/index.ts
@@ -5,5 +5,10 @@ import startServer from "./server/startServer.js";
 const port = process.env.PORT ?? 4000;
 const databaseUrl = process.env.MONGO_DB_URL!;
 
-await connectDatabase(databaseUrl);
-await startServer(+port);
+(async () => {
+  await connectDatabase(databaseUrl);
+})();
+
+(async () => {
+  await startServer(+port);
+})();
